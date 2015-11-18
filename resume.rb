8b78eabed96783bef469ccb0ff2bed1131cec10b
Justin Richter <jrichter@jetfive.com>
2015-11-18 01:46:29 -0600

    rubocop cleanup

--- resume.rb
+++ resume.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
+# My resume in ruby code
 class Resume
-
   def self.name
     'Justin Richter'
   end
@@ -9,7 +9,7 @@ class Resume
   def self.contact
     phone = '(918) 812-3249'
     email = 'jrichter@jetfive.com'
-    return "Cell: #{phone}, Email: #{email}"
+    "Cell: #{phone}, Email: #{email}"
   end
 
   def self.location
@@ -17,41 +17,43 @@ class Resume
     city = 'Bixby'
     state = 'OK'
     zip = '74008'
-    return "#{street}, #{city} #{state} #{zip}"
+    "#{street}, #{city} #{state} #{zip}"
   end
 
   def self.summary
-    current = 'I am currently working at Tulsa Spine and Specialty Hospital as the Lead Radiology Tech.'
-    what_I_want = 'I use ruby to make my PACS Admin job easier. I want to use ruby all the time.'
-    return "#{current} #{what_I_want}"
+    current = 'I am currently working at Tulsa Spine and Specialty Hospital as'\
+              'the Lead Radiology Tech.'
+    what_i_want = 'I use ruby to make my PACS Admin job easier. I want to use'\
+                  'ruby all the time.'
+    "#{current} #{what_i_want}"
   end
 
   def self.experience
-    exp = YAML.load_file('./yaml/exp.yml')
+    YAML.load_file('./yaml/exp.yml')
   end
 
   def self.skills
-    skills = File.open('./text/skills.txt','r').readlines
+    File.open('./text/skills.txt', 'r').readlines
   end
 
   def self.education
-    edu = YAML.load_file('./yaml/edu.yml')
+    YAML.load_file('./yaml/edu.yml')
   end
 
   def self.hobbies
-    hobbies = File.open('./text/hobbies.txt','r').readlines
+    File.open('./text/hobbies.txt', 'r').readlines
   end
 
   def self.links
-    links = YAML.load_file('./yaml/links.yml')
+    YAML.load_file('./yaml/links.yml')
   end
 
   def self.picture
-    picture = File.open('./text/picture.txt', 'r').readlines
-    return picture
+    File.open('./text/picture.txt', 'r').readlines
   end
 
   def self.about
-    'I am married, 11 years! We have two awesome kids, a 9 year old boy and an 8 year old girl. We have two dogs, three cats and fish.'
+    'I am married, 11 years! We have two awesome kids, a 9 year old boy and an'\
+    ' 8 year old girl. We have two dogs, three cats and fish.'
   end
 end
